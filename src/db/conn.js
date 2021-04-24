9877463e65a62a642129a10d5fc977542d16f1a3
Ravi-09 <gravi0970p@gmail.com>
2021-04-25 00:59:29 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
 
-
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(uri, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
