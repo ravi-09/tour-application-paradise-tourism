86d700d38ca45a0e310d9d0bdd463be763b25140
Ravi-09 <gravi0970p@gmail.com>
2021-04-25 11:37:14 +0530

    update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async(req, res, next) => {
     try {
 
         const token = req.cookies.jwt;
-        const verifyUser = jwt.verify(token, process.env.AUTH_KEY);
+        const verifyUser = jwt.verify(token, "gateryudajkliroehulagyuedbcnzuytuianskkljbsbkds");
         const user = await Register.findOne({ _id: verifyUser._id })
         req.token = token;
         req.user = user;
